3dd86cf5638ef69e24787ac3df754c579d88b53b
Ignitz <moritz.pertl@gmx.at>
2023-12-16 13:59:27 +0100

    Moritz renaming, hash funktion for passwords

--- Server/Backend/Encryption/Encryption.py
+++ Server/Backend/Encryption/Encryption.py
@@ -1,17 +1,17 @@
 import time
 import rsa
+import bcrypt
 
 
 class Encryption:
 
-
     def __init__(self):
         pass
 
-    def generate_whole_key(self):
+    def gen_whole_key(self):
         return rsa.newkeys(4096)
 
-    def generate_public_key(self):
+    def gen_public_key(self):
         print("a")
         (public_key, _) = rsa.newkeys(4096)
         return public_key
@@ -25,11 +25,17 @@ class Encryption:
         except:
             return False
 
+    def gen_salt(self):
+        return bcrypt.gensalt()
+
+    def hash_password(self, password, salt):
+        return bcrypt.hashpw(password, salt)
+
 
 if __name__ == "__main__":
     e = Encryption()
     start = time.time()
     #key = e.generate_whole_key()
     #print(e.encrypt("hallo", key))
-    print(e.generate_whole_key())
+    print(e.gen_whole_key())
     print("--- %s seconds ---" % (time.time() - start))
