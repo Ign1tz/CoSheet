68b8aa24e7921557bb6b66785b4ab19f4412a9bf
Ignitz <ignis.hd@gmx.at>
2023-12-24 02:11:02 +0100

    Server Backend hotfix added session support and cors to fix frontend not being able to communicate with backend

--- Server/Server.py
+++ Server/Server.py
@@ -1,9 +1,17 @@
 from flask import Flask, send_from_directory, request, Response, session, redirect, jsonify
+from flask_session import Session
+from flask_cors import CORS
 import os
+import json
+from Backend.Database.Database import Database
 
 app = Flask(__name__)
 app.debug = True
 app._staic_folder = os.path.abspath("static/")
+CORS(app, support_credentials=True)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
 
 @app.route('/default-spreadsheet', methods=['GET'])
