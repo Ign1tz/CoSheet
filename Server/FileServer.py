e04b6619d881535fb884da3e7be32fea4f6adc7e
elimei1 <112986140+elimei1@users.noreply.github.com>
2024-01-03 23:41:38 +0100

    last try

--- Server/FileServer.py
+++ Server/FileServer.py
@@ -13,6 +13,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+
 @app.route('/default-spreadsheet', methods=['GET'])
 def default_spreadsheet_page():
     return send_from_directory('files', "default-spreadsheet.html")
