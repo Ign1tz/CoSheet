e121a9b3de0ac1168644c80374367650d410a221
Ignitz <ignis.hd@gmx.at>
2024-01-03 16:41:33 +0100

    stuff

--- Server/Backend/Database/Database.py
+++ Server/Backend/Database/Database.py
@@ -1,7 +1,47 @@
 from pymongo.mongo_client import MongoClient
 from dotenv import load_dotenv
 import os
+import json
+import requests
+
+
 class Database:
+
+    def get_profile(self, key_pair):
+        r = requests.get("http://menews.site/get-profile",
+                         data=json.dumps(key_pair), headers={
+                "Content-Type": "application/json"})
+        return r.text
+
+    def get_spreadsheet(self, key_pair):
+        r = requests.get("http://menews.site/get-spreadsheet",
+                         data=json.dumps(key_pair), headers={
+                "Content-Type": "application/json"})
+        return r.json()["spreadsheets"]
+
+    def add_profile(self, profile):
+        r = requests.post("http://menews.site/add-profile",
+                          data=json.dumps(profile), headers={
+                "Content-Type": "application/json"})
+
+    def add_spreadsheet(self, spreadsheet):
+        r = requests.post("http://menews.site/add-spreadsheet",
+                          data=json.dumps(spreadsheet), headers={
+                "Content-Type": "application/json"})
+
+    def delete_profile(self, username, password):
+        r = requests.post("http://menews.site/remove-profile",
+                          data=json.dumps({"username": username, "password": password}), headers={
+                "Content-Type": "application/json"})
+        return r
+
+    def delete_spreadsheet(self, title, owner):
+        r = requests.post("http://menews.site/remove-spreadsheet",
+                          data=json.dumps({"title": title, "owner": owner}), headers={
+                "Content-Type": "application/json"})
+
+    """
+    deprecated
     def __init__(self):
         load_dotenv()
         temp_client = MongoClient(os.getenv("DATABASE_LINK"))
@@ -32,9 +72,9 @@ class Database:
         db.spreadsheet_database.delete_one({"Owner": username, "Title": spreadsheet})
 
     def update_one_entry(self, database, old_entry, entry):
-        database.update_one(old_entry, entry)
+        database.update_one(old_entry, entry)"""
 
 
 if __name__ == "__main__":
     data = Database()
-    print(data.get_from_database(data.spreadsheet_database, {"Username": "tester2"}))
+    print(data.delete_profile("test3","test"))
