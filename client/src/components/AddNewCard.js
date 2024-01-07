d781213c2ffb19c0ce1d8bb3bd8337a79b10f3d7
Ignitz <ignis.hd@gmx.at>
2024-01-07 22:45:17 +0100

    added database proxy (for anna)

--- client/src/components/AddNewCard.js
+++ client/src/components/AddNewCard.js
@@ -1,10 +1,14 @@
+import React from "react";
+import {Link} from "react-router-dom";
 
 export default function AddNewCard(props) {
     return (
-        <div className="card">
-            <div className="card-details">
-                <h1>+</h1>
+        <Link to={"/spreadsheet"}>
+            <div className="card">
+                <div className="card-details">
+                    <h1>+</h1>
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
--- client/src/pages/Spreadsheet.js
+++ client/src/pages/Spreadsheet.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import '../Spreadsheet.css';
+import '../Style/Spreadsheet.css';
 
 export default function Spreadsheet() {
     const [numberOfRows, setNumberOfRows] = useState(20);
